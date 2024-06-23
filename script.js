345fa55189b9f03cb689beeb9a8861519740a0ab
Aditi Rastogi <aditi173ras@gmail.com>
2024-06-23 22:09:00 +0530

    updated

--- script.js
+++ script.js
@@ -4,24 +4,21 @@ var blur = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + 30 + "px";
   crsr.style.top = dets.y + "px";
-  blur.style.left = dets.x - 200 + "px";
-  blur.style.top = dets.y - 200 + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
 });
 
 var h4all = document.querySelectorAll("#nav h4");
 h4all.forEach(function (elem) {
   elem.addEventListener("mouseenter", function () {
-    crsr.style.scale = 2.3;
+    crsr.style.scale = 2.2;
     crsr.style.border = "1px solid #fff";
-    
-    crsr.style.scale = 2.5;
-    crsr.style.border = "0.1px solid #fff";
     crsr.style.backgroundColor = "transparent";
   });
   elem.addEventListener("mouseleave", function () {
     crsr.style.scale = 1;
-    crsr.style.border = "0px solid #91b508";
-    crsr.style.backgroundColor = "#91b508";
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
   });
 });
 
@@ -52,7 +49,7 @@ gsap.to("#main", {
 });
 
 gsap.from("#about-us img,#about-us-in", {
-  y: 50,
+  y: 90,
   opacity: 0,
   duration: 1,
   scrollTrigger: {
@@ -60,13 +57,13 @@ gsap.from("#about-us img,#about-us-in", {
     scroller: "body",
     // markers:true,
     start: "top 70%",
-    end: "top 63%",
+    end: "top 65%",
     scrub: 1,
   },
 });
 gsap.from(".card", {
   scale: 0.8,
-  opacity:0,
+  // opacity:0,
   duration: 1,
   stagger: 0.1,
   scrollTrigger: {
@@ -74,7 +71,7 @@ gsap.from(".card", {
     scroller: "body",
     // markers:false,
     start: "top 70%",
-    end: "top 63%",
+    end: "top 65%",
     scrub: 1,
   },
 });
@@ -86,8 +83,8 @@ gsap.from("#colon1", {
     scroller: "body",
     // markers:true,
     start: "top 55%",
-    end: "top 47%",
-    scrub: 3,
+    end: "top 45%",
+    scrub: 4,
   },
 });
 gsap.from("#colon2", {
@@ -99,7 +96,7 @@ gsap.from("#colon2", {
     // markers:true,
     start: "top 55%",
     end: "top 45%",
-    scrub: 3,
+    scrub: 4,
   },
 });
 gsap.from("#page4 h1", {
@@ -108,10 +105,9 @@ gsap.from("#page4 h1", {
     trigger: "#page4 h1",
     scroller: "body",
     // markers:true,
-    start: "top 55%",
-    end: "top 45%",
+    start: "top 75%",
+    end: "top 70%",
     scrub: 3,
   },
 });
-
 
