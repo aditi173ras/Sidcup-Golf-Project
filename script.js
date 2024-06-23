4c4396ad4566ecd4afc5f4da3a0e335c28fde708
Aditi Rastogi <aditi173ras@gmail.com>
2024-06-23 22:31:42 +0530

    updated

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var blur = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + 30 + "px";
   crsr.style.top = dets.y + "px";
-  blur.style.left = dets.x - 250 + "px";
-  blur.style.top = dets.y - 250 + "px";
+  blur.style.left = dets.x - 180 + "px";
+  blur.style.top = dets.y - 180 + "px";
 });
 
 var h4all = document.querySelectorAll("#nav h4");
